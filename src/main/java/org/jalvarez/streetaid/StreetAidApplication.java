620157bffc6005172c5f16d9154f2bace331b27e
jalvarez085 <jalvare9687@student.gwinnetttech.edu>
2026-03-28 12:47:23 -0400

    Intital commit

---
+++ src/main/java/org/jalvarez/streetaid/StreetAidApplication.java
@@ -0,0 +1,13 @@
+package org.jalvarez.streetaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetAidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jalvarez/streetaid/StreetAidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jalvarez.streetaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
